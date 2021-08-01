448114229a70a96508b89c007bf1b77e8f563030
raji_ac <rajiac00@gmail.com>
2021-08-01 11:20:12 +0400

    first commit

---
+++ app/src/main/java/com/raji/app/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.raji.app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
